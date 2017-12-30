703a70d8bab6b7ccc7476cf65a480608a6baf09b
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-30 19:37:15 +0100

    add style and js file to the popup

---
+++ popup/popup.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < localStorage.length; i++){
+    savedpass(localStorage.getItem(localStorage.key(i)));
+}
+
+function savedpass(word) {
+    var x = document.createElement("li");
+    var t = document.createTextNode(word);
+    x.appendChild(t);
+    document.getElementById("myList").appendChild(x);
+}
+
